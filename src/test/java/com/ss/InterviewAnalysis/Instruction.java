bcfa03d174602bdcb7207efc40d07558a1fed361
santhoshkumar <santhoshkumar.elumalai@softsuave.com>
2022-05-16 15:42:46 +0530

    first commit

---
+++ src/test/java/com/ss/InterviewAnalysis/Instruction.java
@@ -0,0 +1,11 @@
+package com.ss.InterviewAnalysis;
+
+public class Instruction {
+	
+	// Use Webdriver manager or use chrome drvier from https://chromedriver.chromium.org/
+	
+	//Get required dependecy from https://mvnrepository.com/ and save it in the pom.xml
+
+
+
+}
